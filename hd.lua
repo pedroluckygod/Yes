c84d481da2c37e4d32f13a742ccee71060da37b2
pedroluckygod <138724609+pedroluckygod@users.noreply.github.com>
2025-08-31 21:09:47 -0300

    Update hd.lua

--- hd.lua
+++ hd.lua
@@ -1,3 +1,4 @@
-cmd = function(cmd)
+local hd = {}
+hd.cmd = function(cmd)
 game:GetService("ReplicatedStorage"):WaitForChild("HDAdminHDClient"):WaitForChild("Signals"):WaitForChild("RequestCommandSilent"):InvokeServer(";".. cmd)
-end
+end)
